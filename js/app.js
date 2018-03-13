13e6257dd92c7714dd12e2b317b869a861bba268
Sascha Dehler <saschadehler@gmail.com>
2018-03-13 11:30:47 +0100

    feat: Initial commit
    Initial game logic added.

--- js/app.js
+++ js/app.js
@@ -1,18 +1,67 @@
-/*
- * Create a list that holds all of your cards
- */
+// To enhance game with more cards just add icons to this array, per icon 2 cards will be added!
+const icons = ['diamond', 'anchor', 'bolt', 'bomb', 'leaf', 'bicycle', 'paper-plane-o', 'cube'];
+const cards = [];
+
+// Create 2 cards per icon
+icons.forEach(function(icon) {
+  cards.push(icon);
+  cards.push(icon);
+});
+
+let container = document.querySelector('.container');
+let moveCounterElement = document.querySelector('.moves');
+let matchCounter = 0;
+let firstClickedCardElement = null;
+let secondClickedCardElement = null;
+
+// Start a new game at page load
+newGame();
+
+document.querySelector('.restart').addEventListener('click', function(event) {
+  newGame();
+});
 
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+
+function newGame() {
+  shuffle(cards);
+
+  moveCounterElement.textContent = 0;
+  matchCounter = 0;
+  firstClickedCardElement = null;
+  secondClickedCardElement = null;
+
+  let newDeckElement = document.createElement('ul');
+  newDeckElement.className = 'deck';
+
+  cards.forEach(function(card) {
+    let newCardElement = document.createElement('li');
+    newCardElement.className = 'card';
+
+    let newIconElement = document.createElement('i');
+    newIconElement.className = 'fa fa-' + card;
+
+    newCardElement.appendChild(newIconElement);
+    newDeckElement.appendChild(newCardElement);
+  });
+
+  let oldDeck = document.querySelector('.deck');
+  if(oldDeck != null) {
+    oldDeck.remove();
+  }
+
+  container.appendChild(newDeckElement);
+
+  newDeckElement.addEventListener('click', function(event) {
+    if (event.target.nodeName === 'LI') {
+      onCardClicked(event.target);
+    }
+  });
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -24,6 +73,74 @@ function shuffle(array) {
 
     return array;
 }
+
+function onCardClicked(cardElement) {
+  if(!cardElement.classList.contains('open')) {
+    cardElement.classList.add('open');
+    onCardOpened(event.target);
+  }
+}
+
+function onCardOpened(cardElement) {
+  if(firstClickedCardElement == null) {
+      firstClickedCardElement = cardElement;
+  } else {
+    secondClickedCardElement = cardElement;
+
+    incrementMoveCounter();
+
+    delayFunctionCall(function() {
+      firstClickedCardElement.firstElementChild.className === secondClickedCardElement.firstElementChild.className
+        ? onCardsMatched() : onCardsNotMatched();
+    });
+  }
+}
+
+function onCardsMatched() {
+  firstClickedCardElement.classList.add('match');
+  secondClickedCardElement.classList.add('match');
+
+  clearClickedCardElements();
+
+  matchCounter ++;
+
+  if(matchCounter === icons.length) {
+    delayFunctionCall(onAllCardsMatched);
+  }
+}
+
+function onCardsNotMatched() {
+  firstClickedCardElement.classList.add('no-match');
+  secondClickedCardElement.classList.add('no-match');
+
+  delayFunctionCall(function() {
+    firstClickedCardElement.classList.remove('no-match');
+    secondClickedCardElement.classList.remove('no-match');
+
+    firstClickedCardElement.classList.remove('open');
+    secondClickedCardElement.classList.remove('open');
+
+    clearClickedCardElements();
+  });
+}
+
+function incrementMoveCounter() {
+  moveCounterElement.textContent = parseInt(moveCounterElement.textContent) + 1;
+}
+
+function onAllCardsMatched() {
+  alert('You WIN!');
+}
+
+function delayFunctionCall(func) {
+  setTimeout(func, 500);
+}
+
+function clearClickedCardElements() {
+  firstClickedCardElement = null;
+  secondClickedCardElement = null;
+}
+
 
 
 /*
